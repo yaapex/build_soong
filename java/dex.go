59fbc231cc4290cb712e010cdbcf28b8da4eb771
LeddaZ <leonardoledda@gmail.com>
2022-08-18 13:12:05 +0200

    soong: Increase Java heap size for r8
    * It started OOMing randomly
    
    Change-Id: I9c748849e3a05809d26afd78444438336361e2c7

--- java/dex.go
+++ java/dex.go
@@ -426,7 +426,7 @@ var r8, r8RE = pctx.MultiCommandRemoteStaticRules("r8",
 		Command: `rm -rf "$outDir" && mkdir -p "$outDir" && ` +
 			`rm -f "$outDict" && rm -f "$outConfig" && rm -rf "${outUsageDir}" && ` +
 			`mkdir -p $$(dirname ${outUsage}) && ` +
-			`$r8Template${config.R8Cmd} ${config.R8Flags} $r8Flags -injars $in --output $outDir ` +
+			`$r8Template${config.R8Cmd} -JXmx6G ${config.R8Flags} $r8Flags -injars $in --output $outDir ` +
 			`--no-data-resources ` +
 			`-printmapping ${outDict} ` +
 			`-printconfiguration ${outConfig} ` +
